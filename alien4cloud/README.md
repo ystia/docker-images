11e36b83ef1f57b7d404d01658b23431b5aa8315
ROSSO, LOIC <loic.rosso@atos.net>
2018-11-30 16:44:53 +0100

    Add alien4cloud Dockerfile and associed scripts

---
+++ alien4cloud/README.md
@@ -0,0 +1,15 @@
+Docker image of alien4cloud http://alien4cloud.github.io/
+
+## How to use it 
+Simply run this command to start an alien4cloud container exposing port 8088 on the host 
+```
+docker run -d --name A4C -p 8088:8088 ystia/alien4cloud
+```
+Connect to http://localhost:8088, default user and password are 'admin'
+
+This container has the capability to connect to a running Yorc server through environment variables. 
+For example, if your Yorc server IP adress is *11.22.33.44* and is configured to listen on port *8888*, you have to start container like this 
+```
+docker run -d --name A4C -p 8088:8088 -e YORC_HOST=11.22.33.44 -e YORC_PORT=8888 ystia/alien4cloud
+```
+The default number of retry (15 sec) and delay (5 sec) is habitually adequate but you are encountering issues, you can set them manualy through environment variables **NB_RETRY** and **PERIOD_SECOND**
---
+++ alien4cloud/scripts/start.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+if [ -z "${YORC_HOST}" -o -z "${YORC_PORT}" ]; then
+    echo "INFO  Script:Undef YORC_HOST or YORC_PORT, launching alien4cloud only."
+    cd alien4cloud && ./alien4cloud.sh
+else
+
+
+if [ -z "${NB_RETRY}" ]; then
+    NB_RETRY=15 ;
+fi
+if [ -z "${PERIOD_SECOND}" ]; then
+    PERIOD_SECOND=5 ;
+fi
+
+cd alien4cloud && nohup ./alien4cloud.sh &
+a4c_pid=$! ;
+
+orch_created=false ;
+orch_configured=false ;
+orch_enabled=false ;
+
+for i in $(seq 1 $NB_RETRY); do
+    if  [ "$orch_created" = true ] && [ "$orch_configured" = true ] && [ "$orch_enabled" = true ]; then
+        "$(date -u +"%Y-%m-%d %H:%M:%S") INFO  Script:Success ! Orchestrator is enabled."
+        break;
+    fi
+
+    echo "$(date -u +"%Y-%m-%d %H:%M:%S") INFO  Script:Trying to connect to orchestrator, attempt $i." ;
+    
+    if [ "$orch_created" = false ]; then
+        bash ./create_orchestrator.bash
+        if [ $? -eq 0 ]; then echo "$(date -u +"%Y-%m-%d %H:%M:%S") INFO  Script:Orchestrator created.";
+            orch_created=true ;
+        else echo "$(date -u +"%Y-%m-%d %H:%M:%S") WARN  Script:Not able to create orchestrator." ;
+        fi
+    fi
+
+    if [ "$orch_configured" = false ] && [ "$orch_created" = true ]; then
+        bash ./configure_orchestrator.bash --yorc-url "http://${YORC_HOST}:${YORC_PORT}"
+        if [ $? -eq 0 ]; then echo "$(date -u +"%Y-%m-%d %H:%M:%S") INFO  Script:Orchestrator configured.";
+            orch_configured=true ;
+        else echo "$(date -u +"%Y-%m-%d %H:%M:%S") WARN  Script:Not able to configure orchestrator." ;
+        fi
+    fi
+    
+    if [ "$orch_enabled" = false ] && [ "$orch_configured" = true ]; then
+        bash ./enable_orchestrator.bash
+        if [ $? -eq 0 ]; then echo "$(date -u +"%Y-%m-%d %H:%M:%S") INFO  Script:Orchestrator enabled.";
+            orch_enabled=true ;
+        else echo "$(date -u +"%Y-%m-%d %H:%M:%S") WARN  Script:Not able to enable orchestrator." ;
+        fi
+    fi
+    
+
+    sleep $PERIOD_SECOND ;
+done
+fi
+
+wait $a4c_pid
