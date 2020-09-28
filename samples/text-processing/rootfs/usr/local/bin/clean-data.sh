8271fc9e0198f78c29d1ef55b9ccfa4cb4a5f06d
Albertin Loic <loic.albertin@atos.net>
2020-09-28 15:49:26 +0200

    Make clean-data.sh more generic

--- samples/text-processing/rootfs/usr/local/bin/clean-data.sh
+++ samples/text-processing/rootfs/usr/local/bin/clean-data.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Cleaning data from $1 to $2..."
-sed -n '/^Acte I\./,/^\*\*\* END OF THIS PROJECT GUTENBERG EBOOK /p;/^\*\*\* END OF THIS PROJECT GUTENBERG EBOOK /q' $1  > $2
-sleep 30
+mkdir -p "$(dirname ${2})"
+sed -n '/^\*\*\* START OF THIS PROJECT GUTENBERG EBOOK/,/^\*\*\* END OF THIS PROJECT GUTENBERG EBOOK /p;/^\*\*\* END OF THIS PROJECT GUTENBERG EBOOK /q' $1  > $2
+sleep 5
 echo "Done..."
